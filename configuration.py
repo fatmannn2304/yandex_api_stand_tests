4fd8e3eb0553f626925472378cc5d539c10fe0e6
Your Name <you@example.com>
2023-05-09 23:10:16 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://4a0fc99c-d9a1-480c-9498-58830357e96e.serverhub.praktikum-services.ru"
+DOC_PATH = "docs"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
